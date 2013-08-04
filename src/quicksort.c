9bad4fefe0e50d9093b3e0e183940b10ac1029c4
Diego Lemos <diegolemosderesende@gmail.com>
2013-08-05 00:27:57 +0200

    First version with green tests.

--- src/quicksort.c
+++ src/quicksort.c
@@ -1,7 +1,77 @@
 #include <stdio.h>
+#include "quicksort.h"
 
-int main()
+int *quicksort(int *numbers, int size)
 {
-	printf("Hello world!");
-	return 0;
+	if(size == 0)
+	{
+		int empty[] = {};
+		return empty;
+	}
+	
+	else if(size == 1)
+	{
+		return numbers;
+	}
+	
+	else
+	{
+		int pivot = rand() % size;
+
+		int smaller_than_pivot_tmp[size];
+		int smaller_than_pivot_length = 0;
+		int greater_than_pivot_tmp[size];
+		int greater_than_pivot_length = 0;
+		
+		for(int i = 0; i < size; i++)
+		{
+			if(i == pivot)
+			{
+				continue;
+			}
+			
+			if(numbers[i] <= numbers[pivot])
+			{
+				smaller_than_pivot_tmp[smaller_than_pivot_length++] = numbers[i];
+			}
+			
+			else
+			{
+				greater_than_pivot_tmp[greater_than_pivot_length++] = numbers[i];
+			}
+		}
+
+		int smaller_than_pivot[smaller_than_pivot_length];
+	       	arraycopy(smaller_than_pivot_tmp, smaller_than_pivot, 0, smaller_than_pivot_length);
+		//free(smaller_than_pivot_tmp);
+
+		int greater_than_pivot[greater_than_pivot_length];
+	       	arraycopy(greater_than_pivot_tmp, greater_than_pivot, 0, greater_than_pivot_length);
+		//free(greater_than_pivot_tmp);
+
+		int *smaller_sorted = quicksort(smaller_than_pivot, smaller_than_pivot_length);
+		int *greater_sorted = quicksort(greater_than_pivot, greater_than_pivot_length);
+
+		int* sorted_numbers = malloc(size * sizeof(int));
+
+		arraycopy(smaller_sorted, sorted_numbers, 0, smaller_than_pivot_length);
+		//free(smaller_sorted);
+
+		sorted_numbers[smaller_than_pivot_length] = numbers[pivot];
+
+		arraycopy(greater_sorted, sorted_numbers, smaller_than_pivot_length + 1, greater_than_pivot_length);
+		//free(greater_sorted);
+
+		return sorted_numbers;
+	}
 }
+
+void
+arraycopy(int* origin, int* destination, int offset, int size)
+{
+	for(int i = offset, j = 0; j < size; i++, j++)
+	{
+		destination[i] = origin[j];
+	}
+}
+
--- tests/check_quicksort.c
+++ tests/check_quicksort.c
@@ -1,5 +1,50 @@
+#include <stdlib.h>
+#include <check.h>
+#include "../src/quicksort.h"
+	
+START_TEST(should_not_sort_empty_input)
+{
+	int empty[] = {};
+	ck_assert_ptr_eq(quicksort(empty, 0), empty);
+}
+END_TEST
+
+START_TEST(when_input_has_one_number_should_return_the_number)
+{
+	int one_number[] = {1};
+	ck_assert_ptr_eq(quicksort(one_number, 1), one_number);
+}
+END_TEST
+
+START_TEST(should_sort_numbers)
+{
+	int unsorted_numbers[] = {3, 2, 1};
+	int sorted_numbers[] = {1, 2, 3};
+	ck_assert(memcmp(quicksort(unsorted_numbers, 3), sorted_numbers, sizeof(3)) == 0);
+}
+END_TEST
+
+Suite*
+quicksort_suite(void)
+{
+	Suite *suite = suite_create("Quicksort");
+	TCase *tc = tcase_create("Sort");
+	tcase_add_test(tc, should_not_sort_empty_input);
+	tcase_add_test(tc, when_input_has_one_number_should_return_the_number);
+	tcase_add_test(tc, should_sort_numbers);
+	suite_add_tcase(suite, tc);
+	return suite;
+}
+
 int
 main (void)
 {
-	  return 0;
+	int number_failed;
+	Suite *s = quicksort_suite();
+	SRunner *sr = srunner_create(s);
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);	
+       
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
